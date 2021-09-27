fbd2072d841502f35ba29dad505384421034c9a0
mandraj <85092017+mandraj@users.noreply.github.com>
2021-09-27 21:37:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+/* function showThis(a, b) {
+  console.log(this);
+  function sum() {
+    console.log(this);
+    return a + b;
+  }
+
+  console.log(sum());
+}
+
+showThis(4, 5);
+
+const obj = {
+  a: 20,
+  b: 15,
+  sum: function() {
+    function shout() {
+      console.log(this);
+    }
+    shout();
+  }
+}; */
+
+//obj.sum();
+
+/* function User(name, id) {
+  this.name = name;
+  this.is = id;
+  this.human = true;
+  this.hello = function() {
+    console.log('Hello' + this.name);
+  };
+}
+
+let ivan = new User('Ivan', 23); */
+
+/* function sayName(surname) {
+  console.log(this);
+  console.log(this.name + '' + surname);
+}
+
+const user = {
+  name: 'John'
+};
+
+sayName.call(user, 'Smith');
+sayName.apply(user, ['Smith']);
+
+function count(num) {
+  return this*num;
+}
+
+const double = count.bind(2);
+console.log(double(3));
+console.log(double(13)); */
+
+// 1) Обычная функция : this = window, но если use strict - undefined
+// 2) Контекст у методов объекта - сам объект
+// 3) this в конструктор и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+  this.style.backgroundColor = 'red';
+});
+
+const obj = {
+  num: 5,
+  sayNumber: function() {
+    const say = () => {
+      console.log(this.num);
+    };
+
+    say();
+  }
+};
+
+obj.sayNumber();
+
+const double = (a) => {
+  return a * 2;
+};
+
+// const double = () => a * 2; тоже самое
